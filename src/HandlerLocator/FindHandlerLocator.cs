4013f478b15e1af689f028e4332672d2bf0c0f22
digitaldias <pedro@digitaldias.com>
2022-12-26 09:12:07 +0100

    Add method to find All handlers

--- src/HandlerLocator/FindHandlerLocator.cs
+++ src/HandlerLocator/FindHandlerLocator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -56,6 +57,43 @@ namespace HandlerLocator
             }
 
             return null;
+        }
+
+        public async Task<IEnumerable<IdentifiedHandler>> FindAllHandlers()
+        {
+            var allHandlers = new List<IdentifiedHandler>();
+            var syntaxRoot = await _workingDocument.GetSyntaxRootAsync();
+            var syntaxNode = syntaxRoot.FindNode(new Microsoft.CodeAnalysis.Text.TextSpan(_linePosition, 0), true, true);
+
+            // Get type information
+            var semanticModel = await _workingDocument.GetSemanticModelAsync();
+
+            var symbol = GetTypeInfo(semanticModel, syntaxNode);
+
+            var references = await SymbolFinder.FindReferencesAsync(symbol, _solution);
+            foreach (var reference in references)
+            {
+                foreach (var location in reference.Locations)
+                {
+                    var tree = await location.Document.GetSyntaxTreeAsync();
+                    var root = await tree.GetRootAsync();
+                    var allMethods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+                    var publicMethods = allMethods.Where(publicMethod =>
+                        publicMethod.Modifiers.Any(modifier => modifier.Text.Equals("public")) &&
+                        publicMethod.ParameterList.Parameters.Any(parameter => parameter.ToFullString().Contains(symbol.Name)));
+
+                    if (!publicMethods.Any())
+                        continue;
+
+                    allHandlers.Add(new IdentifiedHandler
+                    {
+                        SourceFile = location.Document.FilePath,
+                        LineNumber = publicMethods.First().SpanStart
+                    });
+                }
+            }
+
+            return allHandlers;
         }
 
         private ITypeSymbol GetTypeInfo(SemanticModel semanticModel, SyntaxNode syntaxNode)
